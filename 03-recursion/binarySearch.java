e9dad56361cb9c09dff24ce6e6fc79c6db34091b
Seda GÜNEY DURAN <41970059+SedaGUNEYDURAN@users.noreply.github.com>
2025-04-10 15:38:51 +0300

    Update binarySearch.java

--- 03-recursion/binarySearch.java
+++ 03-recursion/binarySearch.java
@@ -1,1 +1,24 @@
-
+public class Main {
+  public static void main(String[] args) {
+  int[] nums={2,4,6,8,72,213,345};
+  int target=8;
+  int right=nums.length-1;
+  int left=0;
+    System.out.println(recursive(nums,target,left,right));
+  }
+  
+  public static int recursive(int[] nums, int target, int left,int right){
+    if(left>right){
+        return -1;
+    }
+    int index=((left+right)/2);
+    int pivot=nums[index];
+ 	  if(pivot==target){
+       return index;
+    } else if(pivot>target){
+       return recursive( nums, target,left,(index-1));
+    } else{
+       return recursive( nums,  target,(index+1),right);
+    }   
+  }
+}
